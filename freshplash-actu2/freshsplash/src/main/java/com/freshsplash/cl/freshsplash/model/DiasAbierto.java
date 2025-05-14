e63d63a4cef118408b481ba2ac63f035abd94c9c
Esteban Gamboa <61128292+baaabymoney@users.noreply.github.com>
2025-05-14 09:54:30 -0400

    Add files via upload

---
+++ freshplash-actu2/freshsplash/src/main/java/com/freshsplash/cl/freshsplash/model/DiasAbierto.java
@@ -0,0 +1,28 @@
+package com.freshsplash.cl.freshsplash.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "DiasAbierto")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiasAbierto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length= 10,nullable=false)
+    private String diaSemana;
+
+}
---
+++ freshplash-actu2/freshsplash/src/main/java/com/freshsplash/cl/freshsplash/service/EtiquetaService.java
@@ -0,0 +1,79 @@
+package com.freshsplash.cl.freshsplash.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.freshsplash.cl.freshsplash.model.Etiqueta;
+import com.freshsplash.cl.freshsplash.repository.EtiquetaRepository;
+
+@Service
+public class EtiquetaService {
+
+    @Autowired
+    private EtiquetaRepository etiquetaRepository;
+
+    private static final int cargo = 500;
+
+    public List<Etiqueta> obtenerEtiquetas() {
+        return etiquetaRepository.findAll();
+    }
+
+    public Etiqueta obtenerEtiquetaPorId(Long id) {
+        return etiquetaRepository.findById(id).orElse(null);
+    }
+
+    public Etiqueta guardarEtiqueta(Etiqueta etiqueta) {
+        if (!etiqueta.isGratuito() && (etiqueta.getPrecio() == null || etiqueta.getPrecio() <= 0)) {
+            etiqueta.setPrecio(cargo);
+        }
+        return etiquetaRepository.save(etiqueta);
+    }
+
+    public void eliminarEtiqueta(Long id) {
+        etiquetaRepository.deleteById(id);
+    }
+
+    public Etiqueta actualizarEtiqueta(Long id, Etiqueta etiqueta) {
+        Etiqueta etiquetaExistente = etiquetaRepository.findById(id).orElse(null);
+        if (etiquetaExistente != null) {
+            etiquetaExistente.setAccesoDiscapacitado(etiqueta.isAccesoDiscapacitado());
+            etiquetaExistente.setGratuito(etiqueta.isGratuito());
+
+            if (!etiqueta.isGratuito() && (etiqueta.getPrecio() == null || etiqueta.getPrecio() <= 0)) {
+                etiquetaExistente.setPrecio(cargo);
+            } else {
+                etiquetaExistente.setPrecio(etiqueta.getPrecio());
+            }
+
+            return etiquetaRepository.save(etiquetaExistente);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean esAccesibleParaDiscapacitados(Long id) {
+        Etiqueta etiqueta = etiquetaRepository.findById(id).orElse(null);
+        return etiqueta != null && etiqueta.isAccesoDiscapacitado();
+    }
+
+    public boolean esGratuito(Long id) {
+        Etiqueta etiqueta = etiquetaRepository.findById(id).orElse(null);
+        return etiqueta != null && etiqueta.isGratuito();
+    }
+
+    public Etiqueta actualizarPrecio(Long id, Integer nuevoPrecio) {
+        Etiqueta etiquetaExistente = etiquetaRepository.findById(id).orElse(null);
+        if (etiquetaExistente != null) {
+            if (nuevoPrecio != null && nuevoPrecio > 0) {
+                etiquetaExistente.setPrecio(nuevoPrecio);
+            } else {
+                etiquetaExistente.setPrecio(cargo);
+            }
+            return etiquetaRepository.save(etiquetaExistente);
+        } else {
+            return null;
+        }
+    }
+}
